fc1e0f4cf3e187dcf9a7042d3bf74f30154c39c0
Garrett <gn9012@gmail.com>
2017-02-27 16:04:02 +0000

    consolidate character models

--- characters/admin.py
+++ characters/admin.py
@@ -4,13 +4,7 @@ from . import models
 
 
 class CharacterAdmin(admin.ModelAdmin):
-  list_display = ('name', 'user', 'level',)
-  list_filter = ('user', 'level',)
-
-class PlayerAdmin(admin.ModelAdmin):
-  list_display = ('character_name', 'player_name', 'user',)
+  list_display = ('name', 'user',)
   list_filter = ('user',)
 
-admin.site.register(models.Player, PlayerAdmin)
-admin.site.register(models.Monster, CharacterAdmin)
-admin.site.register(models.NPC, CharacterAdmin)
+admin.site.register(models.Character, CharacterAdmin)
--- characters/models.py
+++ characters/models.py
@@ -55,6 +55,34 @@ class Character(models.Model):
     speed = models.CharField(max_length=255, default='', blank=True)
     saving_throws = models.CharField(max_length=255, default='', blank=True)
     skills = models.CharField(max_length=255, default='', blank=True)
+    creature_type = models.CharField(max_length=255, default='', blank=True)
+    damage_vulnerabilities = models.CharField(max_length=255, default='', blank=True)
+    damage_immunities = models.CharField(max_length=255, default='', blank=True)
+    damage_resistances = models.CharField(max_length=255, default='', blank=True)
+    condition_immunities = models.CharField(max_length=255, default='', blank=True)
+    senses = models.CharField(max_length=255, default='', blank=True)
+    challenge_rating = models.CharField(max_length=255, default='', blank=True)
+    traits = models.TextField(blank=True)
+    actions = models.TextField(blank=True)
+    notes = models.TextField(blank=True)
+    character_class = models.CharField(verbose_name= _('Class'),max_length=255, default='', blank=True)
+    age = models.CharField(max_length=255, default='', blank=True)
+    height = models.CharField(max_length=255, default='', blank=True)
+    weight = models.CharField(max_length=255, default='', blank=True)
+    proficiency_bonus = models.CharField(max_length=255, default='', blank=True)
+    race = models.CharField(max_length=255, default='', blank=True)
+    xp = models.IntegerField(verbose_name= _('XP'), default=0, blank=True)
+    background = models.CharField(max_length=255, default='', blank=True)
+    initiative = models.CharField(max_length=255, default='', blank=True)
+    personality = models.TextField(blank=True)
+    bonds = models.TextField(blank=True)
+    ideals = models.TextField(blank=True)
+    flaws = models.TextField(blank=True)
+    feats = models.TextField(blank=True)
+    attacks = models.TextField(blank=True)
+    spells = models.TextField(blank=True)
+    proficiencies = models.TextField(blank=True)
+    equipment = models.TextField(blank=True)
 
     @property
     def strength_mod(self):
@@ -80,87 +108,10 @@ class Character(models.Model):
     def charisma_mod(self):
         return score_to_mod_string(self.charisma)
 
-    class Meta:
-        abstract = True
-
     def __str__(self):
         return self.name
-
-
-class Monster(Character):
-    creature_type = models.CharField(max_length=255, default='', blank=True)
-    damage_vulnerabilities = models.CharField(max_length=255, default='', blank=True)
-    damage_immunities = models.CharField(max_length=255, default='', blank=True)
-    damage_resistances = models.CharField(max_length=255, default='', blank=True)
-    condition_immunities = models.CharField(max_length=255, default='', blank=True)
-    senses = models.CharField(max_length=255, default='', blank=True)
-    challenge_rating = models.CharField(max_length=255, default='', blank=True)
-    traits = models.TextField(blank=True)
-    actions = models.TextField(blank=True)
-    notes = models.TextField(blank=True)
 
     def get_absolute_url(self):
         return reverse('characters:monster_detail', kwargs={
             'monster_pk': self.pk
             })
-
-
-class NPC(Character):
-    npc_class = models.CharField(verbose_name= _('Class'),max_length=255, default='', blank=True)
-    age = models.CharField(max_length=255, default='', blank=True)
-    height = models.CharField(max_length=255, default='', blank=True)
-    weight = models.CharField(max_length=255, default='', blank=True)
-    creature_type = models.CharField(max_length=255, default='', blank=True)
-    damage_vulnerabilities = models.CharField(max_length=255, default='', blank=True)
-    damage_immunities = models.CharField(max_length=255, default='', blank=True)
-    damage_resistances = models.CharField(max_length=255, default='', blank=True)
-    condition_immunities = models.CharField(max_length=255, default='', blank=True)
-    senses = models.CharField(max_length=255, default='', blank=True)
-    challenge_rating = models.CharField(max_length=255, default='', blank=True)
-    traits = models.TextField(blank=True)
-    actions = models.TextField(blank=True)
-    notes = models.TextField(blank=True)
-
-    class Meta:
-        verbose_name = 'NPC'
-        verbose_name_plural = 'NPCs'
-
-    def get_absolute_url(self):
-        return reverse('characters:npc_detail', kwargs={
-            'npc_pk': self.pk
-            })
-
-
-class Player(Character):
-    character_name = models.CharField(max_length=255, default='')
-    proficiency_bonus = models.CharField(max_length=255, default='', blank=True)
-    player_name = models.CharField(max_length=255, default='', blank=True)
-    character_class = models.CharField(max_length=255, default='', blank=True)
-    race = models.CharField(max_length=255, default='', blank=True)
-    xp = models.IntegerField(verbose_name= _('XP'), default=0, blank=True)
-    background = models.CharField(max_length=255, default='', blank=True)
-    age = models.CharField(max_length=255, default='', blank=True)
-    height = models.CharField(max_length=255, default='', blank=True)
-    weight = models.CharField(max_length=255, default='', blank=True)
-    initiative = models.CharField(max_length=255, default='', blank=True)
-    personality = models.TextField(blank=True)
-    bonds = models.TextField(blank=True)
-    ideals = models.TextField(blank=True)
-    flaws = models.TextField(blank=True)
-    feats = models.TextField(blank=True)
-    attacks = models.TextField(blank=True)
-    spells = models.TextField(blank=True)
-    notes = models.TextField(blank=True)
-    traits = models.TextField(blank=True)
-    proficiencies = models.TextField(blank=True)
-    senses = models.CharField(max_length=255, default='', blank=True)
-    equipment = models.TextField(blank=True)
-
-
-
-
-    def get_absolute_url(self):
-        return reverse('characters:player_detail', kwargs={
-            'player_pk': self.pk
-            })
-    
